9d4c5c624e8b289fd77bf662ca481b7b12603173
Sean Morrison <seanmorrison112358@gmail.com>
2014-12-20 20:09:56 -0800

    Add magnitude and direction methods to ship; add test rotation.

--- app/src/main.js
+++ app/src/main.js
@@ -7,30 +7,58 @@ define(function(require, exports, module) {
     var Transform = require('famous/core/Transform');
     var ImageSurface = require('famous/surfaces/ImageSurface');
     var StateModifier = require('famous/modifiers/StateModifier');
+    var PhysicsEngine = require ('famous/physics/PhysicsEngine');
+    var Circle = require('famous/physics/bodies/Circle');
+    var Body = require('famous/physics/bodies/Body');
     // create the main context
     var mainCon = Engine.createContext();
 
     // your app here
-
+    var physicsEng = new PhysicsEngine();
 
     mainCon.setPerspective(1000);
 
+    var shipArray = [];
     var Ship = function Ship(shipAlign, shipOrigin) {
       this.surface = new ImageSurface({
         size:[52,52],
         content: '/content/images/AsteroidsShip_color.gif'
       });
-      this.magnitude = 0.0;
-      this.direction = 0;
       this.alignment = shipAlign;
       this.state = new StateModifier({
         align: shipAlign,
         origin: shipOrigin
       });
+      this.particle = new Circle({radius:20});
+      this.magnitude = 0.0;
+      this.direction = 0;
+      this.getMagnitude = function() {
+        return Math.sqrt( ((this.particle.getVelocity()[0]) * (this.particle.getVelocity()[0])) + ((this.particle.getVelocity()[1]) * (this.particle.getVelocity()[1])) );
+      };
+      this.getDirection = function() {
+        var direction = Math.atan2((-1 * this.particle.getVelocity()[1]), this.particle.getVelocity()[0]);
+        direction = direction * (180 / Math.PI);
+        if (this.particle.getVelocity()[1] > 0) {
+          direction = direction + 360;
+        }
+        return direction;
+      };
+      this.setMagnitudeAndDirection = function(magnitude, direction) {
+        direction = direction * (Math.PI / 180);
+        var xComp = magnitude * Math.cos(direction);
+        var yComp = -1 * magnitude * Math.sin(direction);
+        this.particle.setVelocity([xComp,yComp,0]);
+      };
     };
-    var ship0 = new Ship([0.5,0.5],[0.5,0.5]);
 
-    mainCon.add(ship0.state).add(ship0.surface);
+    var testRotationModifier = new StateModifier({
+      transform: Transform.rotateZ(Math.PI/2)
+    });
+
+    var ship0 = new Ship([0.5,0.5],[0.5,0.5]);
+    physicsEng.addBody(ship0.particle);
+    shipArray.push(ship0);
+    mainCon.add(ship0.state).add(testRotationModifier).add(ship0.surface);
 
     // var logo = new ImageSurface({
     //     size: [200, 200],
